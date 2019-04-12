277450cf85a643c5472506eecae6d0d20313640c
方元岭 <manmi9999@gmail.com>
2019-04-12 21:29:43 +0800

    TEST6

---
+++ app/src/main/java/com/will/gps/Base/MainALiSms.java
@@ -0,0 +1,109 @@
+package com.will.gps.Base;
+
+/**
+ * Created by MaiBenBen on 2019/4/11.
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+//import java.util.UUID;
+
+public class MainALiSms {
+    public static void SendSms(String phone, String code) throws Exception {
+        String accessKeyId = "LTAIyTeZnhOVG8eb";
+        String accessSecret = "TtEyCT42D9QO48miTsCTaHN8NVcBwl";
+
+        java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        df.setTimeZone(new java.util.SimpleTimeZone(0, "GMT"));// 这里一定要设置GMT时区
+
+        java.util.Map<String, String> paras = new java.util.HashMap<String, String>();
+        // 1. 系统参数
+        paras.put("SignatureMethod", "HMAC-SHA1");
+        paras.put("SignatureNonce", java.util.UUID.randomUUID().toString());
+        paras.put("AccessKeyId", accessKeyId);
+        paras.put("SignatureVersion", "1.0");
+        paras.put("Timestamp", df.format(new java.util.Date()));
+        paras.put("Format", "XML");
+
+        // 2. 业务API参数
+        paras.put("Action", "SendSms");
+        paras.put("Version", "2017-05-25");
+        paras.put("RegionId", "cn-hangzhou");
+        paras.put("PhoneNumbers", phone);
+        paras.put("SignName", "GPS签到");
+        paras.put("TemplateParam", "{\"code\":\""+code+"\"}");
+        paras.put("TemplateCode", "SMS_163050844");
+        paras.put("OutId", "123");
+
+        // 3. 去除签名关键字Key
+        if (paras.containsKey("Signature"))
+            paras.remove("Signature");
+
+        // 4. 参数KEY排序
+        java.util.TreeMap<String, String> sortParas = new java.util.TreeMap<String, String>();
+        sortParas.putAll(paras);
+
+        // 5. 构造待签名的字符串
+        java.util.Iterator<String> it = sortParas.keySet().iterator();
+        StringBuilder sortQueryStringTmp = new StringBuilder();
+        while (it.hasNext()) {
+            String key = it.next();
+            sortQueryStringTmp.append("&").append(specialUrlEncode(key)).append("=").append(specialUrlEncode(paras.get(key)));
+        }
+        String sortedQueryString = sortQueryStringTmp.substring(1);// 去除第一个多余的&符号
+
+        StringBuilder stringToSign = new StringBuilder();
+        stringToSign.append("GET").append("&");
+        stringToSign.append(specialUrlEncode("/")).append("&");
+        stringToSign.append(specialUrlEncode(sortedQueryString));
+
+        String sign = sign(accessSecret + "&", stringToSign.toString());
+        // 6. 签名最后也要做特殊URL编码
+        String signature = specialUrlEncode(sign);
+
+        System.out.println(paras.get("SignatureNonce"));
+        System.out.println("\r\n=========\r\n");
+        System.out.println(paras.get("Timestamp"));
+        System.out.println("\r\n=========\r\n");
+        System.out.println(sortedQueryString);
+        System.out.println("\r\n=========\r\n");
+        System.out.println(stringToSign.toString());
+        System.out.println("\r\n=========\r\n");
+        System.out.println(sign);
+        System.out.println("\r\n=========\r\n");
+        System.out.println(signature);
+        System.out.println("\r\n=========\r\n");
+        // 最终打印出合法GET请求的URL
+        System.out.println("http://dysmsapi.aliyuncs.com/?Signature=" + signature + sortQueryStringTmp);
+        String u = "http://dysmsapi.aliyuncs.com/?Signature=" + signature + sortQueryStringTmp;
+        System.out.println(u);
+        try {
+            URL url = new URL(u);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+            connection.disconnect();
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("请求失败!");
+        }
+    }
+    public static String specialUrlEncode(String value) throws Exception {
+        return java.net.URLEncoder.encode(value, "UTF-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~");
+    }
+    public static String sign(String accessSecret, String stringToSign) throws Exception {
+        javax.crypto.Mac mac = javax.crypto.Mac.getInstance("HmacSHA1");
+        mac.init(new javax.crypto.spec.SecretKeySpec(accessSecret.getBytes("UTF-8"), "HmacSHA1"));
+        byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+        return new sun.misc.BASE64Encoder().encode(signData);
+    }
+}
+
